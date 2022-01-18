bba08d4ba983525cfb66249973f4494697490009
Berkanktk <berkank@live.dk>
2022-01-18 17:57:56 +0100

    Added finale project files

---
+++ Downloader.py
@@ -0,0 +1,104 @@
+import pytube.exceptions
+from pytube import YouTube
+from pathlib import Path
+import os
+
+'''
+    Author: Berkan Kütük
+    Username: Berkanktk
+'''
+
+
+def downloader():
+    flag = True
+
+    # Standard download folder
+    destination = str(os.path.join(Path.home(), "Downloads"))
+
+    while flag:
+        try:
+            # Url input from the user
+            link = YouTube(input("Enter the URL: \n>> "))
+
+            # Choose download format
+            choose = input("Which format do you prefer: \n1. MP3\n2. MP4\n3. Help\n>> ")
+
+            if choose == "1":
+                print("Extracting....")
+
+                try:
+                    # Extracting the audio only
+                    video = link.streams.filter(only_audio=True).first()
+
+                    print("Downloading....")
+
+                    # Downloading the file
+                    out_file = video.download(output_path=destination)
+
+                    print("Saving....")
+
+                    # Saving the file
+                    base, ext = os.path.splitext(out_file)
+                    new_file = base + '.mp3'
+                    os.rename(out_file, new_file)
+
+                    # Result of success
+                    print("'" + link.title + "'" + " has been successfully downloaded.")
+                    print("*******************************************")
+
+                except FileExistsError:
+                    print("Download failed. File already exists.")
+                    print("*******************************************")
+                except pytube.exceptions.VideoUnavailable:
+                    print("Download failed. The video is unavailable.")
+                    print("*******************************************")
+                finally:
+                    anotherMedia()
+
+            elif choose == "2":
+                print("Extracting....")
+
+                try:
+                    # Extracting the video in highest resolution
+                    video = link.streams.get_highest_resolution()
+
+                    print("Downloading....")
+
+                    # Downloading the video
+                    video.download(destination)
+
+                    print("Saving....")
+
+                    # Result of success
+                    print("'" + link.title + "'" + " has been successfully downloaded.")
+                    print("*******************************************")
+
+                    flag = False
+                except pytube.exceptions.VideoUnavailable:
+                    print("Download failed. The video is unavailable.")
+                finally:
+                    anotherMedia()
+
+            elif choose == "3":
+                print("*******************************************")
+                print("MP3 = Music\nMP4 = Video\n\nThe downloaded files can be found under the 'Downloads' folder")
+                print("*******************************************")
+            else:
+                raise SystemExit
+        except pytube.exceptions.RegexMatchError:
+            print("The URL is invalid.")
+            print("*******************************************")
+
+
+def anotherMedia():
+    another_media = input("Do you wanna download another media?[y/n]\n")
+
+    if another_media == "y":
+        downloader()
+    elif another_media == "n":
+        raise SystemExit
+
+
+if __name__ == '__main__':
+    downloader()
+    anotherMedia()
